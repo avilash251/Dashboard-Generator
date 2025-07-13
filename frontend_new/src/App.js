7f550a968dea54213badedcbef41214601ac853f
avilash251 <avilash251@gmail.com>
2025-07-13 18:07:43 +0530

    Update App.js

--- frontend_new/src/App.js
+++ frontend_new/src/App.js
@@ -1,46 +1,21 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 import ChatPopup from './components/ChatPopup';
-import FloatingBot from './components/FloatingBot';
 
-function AppWrapper() {
-  const [showPopup, setShowPopup] = useState(localStorage.getItem('popupVisible') === 'true');
-  const navigate = useNavigate();
-
-  const openPopup = () => {
-    setShowPopup(true);
-    localStorage.setItem('popupVisible', 'true');
-  };
-
-  const closePopup = () => {
-    setShowPopup(false);
-    localStorage.setItem('popupVisible', 'false');
-    navigate('/');
-  };
-
-  const maximizePopup = () => {
-    setShowPopup(false);
-    localStorage.setItem('popupVisible', 'false');
-    navigate('/promethus-dashboard');
-  };
-
-  return (
-    <>
-      <FloatingBot onClick={openPopup} />
-      {showPopup && <ChatPopup onClose={closePopup} onMaximize={maximizePopup} />}
-      <Routes>
-        <Route path="/" element={<div />} />
-        <Route path="/promethus-dashboard" element={<Dashboard />} />
-      </Routes>
-    </>
-  );
-}
-
-export default function App() {
+function App() {
   return (
     <Router>
-      <AppWrapper />
+      <Routes>
+        <Route path="/prometheus-dashboard" element={<Dashboard />} />
+        {/* Default page */}
+        <Route path="/" element={
+          <>
+            <ChatPopup onExpand={() => window.location.href = "/prometheus-dashboard"} />
+          </>
+        } />
+      </Routes>
     </Router>
   );
 }
+
+export default App;
