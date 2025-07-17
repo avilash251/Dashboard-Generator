10b62857d4968c9625372792d5d9eddf575a1208
avilash251 <avilash251@gmail.com>
2025-07-17 19:31:11 +0530

    Create websocket_endpoint.py

---
+++ backend/ws/websocket_endpoint.py
@@ -0,0 +1,8 @@
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await ws_manager.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()  # optional, just to keep alive
+    except:
+        ws_manager.disconnect(websocket)
